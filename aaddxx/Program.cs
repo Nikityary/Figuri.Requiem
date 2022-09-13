42a780158b005c0341e7ecf3afc8f35f8cffc5d2
Никита <nikityary@mail.ru>
2022-09-13 20:24:37 +0300

    Добавьте файлы проекта.

---
+++ aaddxx/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figuri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Shape[] shapes = new Shape[2];
+                shapes[0] = new Circle(3, 5, "зелёного");
+                shapes[1] = new Rectangle(6, 7, 6, 7, "красного");
+
+                foreach (Shape shape in shapes)
+                {
+                    shape.draw();
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+    
+}
